15ca0b0577767a50f16b4ff9c8e1aae73575621b
Mike_Zhang1 <751603405@qq.com>
2021-01-16 00:38:02 +0800

    'blog-backend'

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fe506f943db52e3d491d2d5d15fa4863'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9932b5e7-669e-4833-ae20-d72437872ec6'
+};
