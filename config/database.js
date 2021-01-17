bbb1cb97b931ae0327c96b873163f19319b6c0b0
Mike_Zhang1 <751603405@qq.com>
2021-01-17 10:53:49 +0800

    '修改域名'

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', '106.75.179.193'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'blog'),
         username: env('DATABASE_USERNAME', 'blog'),
